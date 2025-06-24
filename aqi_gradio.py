32ed4b877a3f26175dcd41ac953e00e84b6f33a4
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2025-06-24 14:12:54 +0530

    Models: AQIResp Struct, UserStruct

--- aqi_gradio.py
+++ aqi_gradio.py
@@ -13,3 +13,20 @@ class AQIResponse(BaseModel):
     data: Dict[str, float]
     status: str
     expiresAt: str
+    
+class ExtractSchema(BaseModel):
+    aqi: float = Field(description = "Air Quality Index")
+    temperature: float = Field(description = "Temperature in Degree Celsius")
+    humidity: float = Field(description = "Humidity Percentage")
+    wind_speed: float = Field(description = "")
+    pm25:float = Field(description = "Particulate Matter 2.5 micrometers")
+    pm10:float = Field(description = "Particulate Matter 10 micrometers")
+    co: float = Field(description = "Carbon Monoxide Level")
+    
+@dataclass
+class UserInput:
+    city: str
+    state: str
+    country: str
+    medical_conditions: Optional[str]
+    planned_activity: str
