01f079ceab86a2e1341b20bb63b6e79ec42dd06f
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2025-06-24 14:56:01 +0530

    data_fetch, format_url

--- aqi_gradio.py
+++ aqi_gradio.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 import gradio as gr
 from agno.agent import Agent
 from dotenv import load_dotenv
@@ -39,4 +40,35 @@ class AQIAnalyzer:
     def __init__(self, firecrawl_key : str) -> None:
         self.firecrawl = FirecrawlApp(api_key = os.getenv("firecrawl_key"))
     
+    def _format_url(self, country : str, state: str, city: str) -> str:
+        """Format URLs based on location, handling cases with and without state
+        """
+        country_clean = country.lower().replace(" ", "-")
+        city_clean = city.lower().replace(" ", "-")
+        
+        if not state or state.lower().replace(" ","-"):
+            return f"https://www.aqi.in/dashboard/{country_clean}/{city_clean}"
+        
+        state_clean = state.lower().replace(" ", "-")
+        return f"https://www.aqi.in/dashboard/{country_clean}/{state_clean}/{city_clean}"
+        
     
+    def fetch_aqi_data(self, city: str, state: str, country: str) -> tuple[Dict[str, float], str]:
+        """Fetch API data using Firecrawl"""
+        url = self._format_url(country, state, city)
+        info_msg = f"Accessing URL: {url}"
+        
+        resp = self.firecrawl.extract(
+            urls = [f"{url}/*"],
+            params = {
+                "prompt" : "Extract the current real-time AQI, temperature, humidity, wind speed, PM2.5, PM10 and CO Levels from the page. Also extract the timestamp of the data.",
+                "schema": ExtractSchema.model_json_schema()
+            }
+        )
+        
+        aqi_response = AQIResponse(**resp)
+        
+        if not aqi_response.success:
+            raise requests.HTTPError(f"Failed to fetch AQI Data: {aqi_response.status}")
+        
+        return aqi_response.data, info_msg
